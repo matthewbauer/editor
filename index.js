ba9ac1f361a2d799eb3c99e80c933cea5119a86e
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-09-06 17:31:51 -0500

    Rework index to use editor and tree views.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+require('./index.css!')
+
 var tree = document.createElement('iframe')
+tree.style.width = '15%'
 tree.setAttribute('src', './tree')
 document.body.appendChild(tree)
 
+var editor = document.createElement('iframe')
+editor.style.width = '85%'
+document.body.appendChild(editor)
+
 window.addEventListener('message', function(event) {
   var data = JSON.parse(event.data)
-  if (data.type === 'open') {
-    var editor = document.createElement('iframe')
-    editor.setAttribute('src', './editor?' + data.path)
-    document.body.appendChild(editor)
-  }
+  if (data.type === 'open')
+    editor.setAttribute('src', data.href)
 })
--- tree.js
+++ tree.js
@@ -1,18 +1,10 @@
+require('./tree.css!')
+
 var sharejs = require('share/lib/client/index')
 var denodeify = require('denodeify')
 
-function click(event) {
-  if (window.parent !== window) {
-    event.preventDefault()
-    window.parent.postMessage(JSON.stringify({
-      type: 'open',
-      path: event.target.getAttribute('href')
-    }), '*')
-  }
-}
-
 function remove(file) {
-  var parts = file.split('/').slice(1)
+  var parts = file.split('/')
   var el = parts.reduce(function(prev, part){
     return prev.getElementsByClassName(part)[0]
   }, document.body)
@@ -21,14 +13,23 @@ function remove(file) {
 }
 
 function add(file) {
-  var parts = file.split('/').slice(1)
+  var parts = file.split('/')
   parts.reduce(function(prev, part){
     var el = prev.getElementsByClassName(part)[0]
     if (!el) {
       var link = document.createElement('a')
       link.textContent = part
-      link.setAttribute('href', file)
-      link.addEventListener('click', click)
+      link.setAttribute('href', 'editor?' + file)
+      link.setAttribute('target', '_blank')
+      link.addEventListener('click', function(event) {
+        if (window.parent !== window) {
+          event.preventDefault()
+          window.parent.postMessage(JSON.stringify({
+            type: 'open',
+            href: event.target.getAttribute('href')
+          }), '*')
+        }
+      })
 
       el = document.createElement('div')
       el.appendChild(link)
@@ -46,16 +47,24 @@ var proto = 'ws'
 var socket = new WebSocket(proto + '://' + location.hostname + ':' + location.port)
 var share = new sharejs.Connection(socket)
 
-var query = share.createSubscribeQuery(COLLECTION, {}, {})
-query.on('insert', function(docs, idx) {
-  console.log('insert')
-})
-query.on('remove', function(docs, idx) {
-  console.log('remove')
-})
-query.on('move', function(docs, from, to) {
-  console.log('move')
-})
-query.on('change', function() {
-  console.log('change')
+share.on('connected', function() {
+  var query = share.createSubscribeQuery(COLLECTION, {}, {docMode:'sub'}, function(err, documents) {
+    documents.forEach(function(doc) {
+      add(doc.name)
+    })
+  })
+  query.on('insert', function(documents) {
+    documents.forEach(function(doc) {
+      add(doc.name)
+    })
+  })
+  query.on('remove', function(documents) {
+    documents.forEach(function(doc) {
+      remove(doc.name)
+    })
+  })
+  query.on('move', function(data) {
+  })
+  query.on('change', function(data) {
+  })
 })
