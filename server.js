b593142dfa147fc856dc757ce5b49a6755ec47ae
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-09-05 20:34:33 -0500

    use websockets-stream

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var fs = require('fs')
 var http = require('http')
 var chokidar = require('chokidar')
 var denodeify = require('denodeify')
-var WebSocketServer = require('ws').Server
 var gitignore = require('gitignore-parser')
 var ecstatic = require('ecstatic')
 var livedb = require('livedb')
 var share = require('share')
 var Duplex = require('stream').Duplex
 var opn = require('opn')
+var websocket = require('websocket-stream')
 
 var sockets = []
 
@@ -21,37 +21,11 @@ server.on('request', function(request, response) {
   ec(request, response)
 })
 
-function getStream(socket) {
-  var stream = new Duplex({objectMode: true})
-  stream._write = function(chunk, encoding, callback) {
-    socket.send(JSON.stringify(chunk))
-    callback()
-  }
-  stream._read = function() {}
-  stream.headers = socket.upgradeReq.headers
-  stream.remoteAddress = socket.upgradeReq.connection.remoteAddress
-  socket.on('message', function(data) {
-    stream.push(JSON.parse(data))
-  })
-  stream.on('error', function(message) {
-    socket.close(message)
-  })
-  socket.on('close', function(reason) {
-    stream.push(null)
-    stream.emit('close')
-    socket.close(reason)
-  })
-  stream.on('end', function() {
-    socket.close()
-  })
-  return stream
-}
-
 var backend = livedb.client(livedb.memory())
 var shareClient = share.server.createClient({backend: backend})
-var wss = new WebSocketServer({server: server})
-wss.on('connection', function(socket) {
-  shareClient.listen(getStream(socket))
+
+var wss = websocket.createServer({server: server}, function(stream) {
+  shareClient.listen(stream)
 })
 
 var ignore
