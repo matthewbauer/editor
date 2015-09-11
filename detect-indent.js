7686b3d13bd118123ec90264710df77c113e9dc1
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-09-11 01:22:14 -0500

    Remove detect-indent config for tabSize.

--- detect-indent.js
+++ detect-indent.js
@@ -6,7 +6,6 @@ var detectIndent = require('detect-indent')
 function setIndent(editor) {
   var indent = detectIndent(editor.getValue())
   editor.setOption('indentUnit', indent.amount)
-  editor.setOption('tabSize', indent.amount)
   editor.setOption('indentWithTabs', indent.type === 'tab')
 }
 
